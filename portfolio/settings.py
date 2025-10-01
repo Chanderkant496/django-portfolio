745a9ddd3c044297a0aa49e80ec5741ec70ea9f0
Chanderkant496 <ckdhiman496@gmail.com>
2025-10-01 23:04:47 +0530

    add

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-04j%o+_zg5kj&*5%tfu90l__2cdlo0v0p-dv#^vyv0o=9m==_5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['chanderkant.pyhtonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
