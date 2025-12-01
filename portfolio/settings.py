dfadc1aae4c77dfe67e6490d88adcc9901c69b00
Chanderkant496 <ckdhiman496@gmail.com>
2025-12-01 20:30:59 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-04j%o+_zg5kj&*5%tfu90l__2cdlo0v0p-dv#^vyv0o=9m==_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-portfolio.onrender.com"]
+ALLOWED_HOSTS = [
+    '.onrender.com',  # Allows any subdomain of onrender.com
+]
+
 
 
 
